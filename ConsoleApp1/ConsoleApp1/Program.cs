190d69996b220dacaf0bd585e04ae16331579ef0
Timothy Hansen <30021573@student.toiohomai.ac.nz>
2019-04-10 10:07:12 +1200

    test 1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+            //test 1//
+           double num;
+        int exponent;
+        {
+            
+            Console.WriteLine("Enter the value of the num");
+            double num = double.Parse(Console.ReadLine)
+
+
+
+            Console.WriteLine("enter the value of the exponent");
+               
+     
+
+        }
+    }
+}
